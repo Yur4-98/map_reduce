e65669b44d83f420d1cfe8f6ce689e71fcf51f72
Yur4-98 <yura-kasyan@mail.ru>
2024-01-30 21:44:03 +0300

    more projects

--- manager/manager.cpp
+++ manager/manager.cpp
@@ -1,20 +1,92 @@
-﻿// manager.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#ifdef WIN32
+#define _WIN32_WINNT 0x0501
+#include <stdio.h>
+#endif
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/enable_shared_from_this.hpp>
+using namespace boost::asio;
+using namespace boost::posix_time;
+io_service service;
+
+#define MEM_FN(x)       boost::bind(&self_type::x, shared_from_this())
+#define MEM_FN1(x,y)    boost::bind(&self_type::x, shared_from_this(),y)
+#define MEM_FN2(x,y,z)  boost::bind(&self_type::x, shared_from_this(),y,z)
+
+
+class talk_to_client : public boost::enable_shared_from_this<talk_to_client>, boost::noncopyable {
+    typedef talk_to_client self_type;
+    talk_to_client() : sock_(service), started_(false) {}
+public:
+    typedef boost::system::error_code error_code;
+    typedef boost::shared_ptr<talk_to_client> ptr;
+
+    void start() {
+        started_ = true;
+        do_read();
+    }
+    static ptr new_() {
+        ptr new_(new talk_to_client);
+        return new_;
+    }
+    void stop() {
+        if (!started_) return;
+        started_ = false;
+        sock_.close();
+    }
+    ip::tcp::socket& sock() { return sock_; }
+private:
+    void on_read(const error_code& err, size_t bytes) {
+        if (!err) {
+            std::string msg(read_buffer_, bytes);
+            // echo message back, and then stop
+            do_write(msg + "\n");
+        }
+        stop();
+    }
+
+    void on_write(const error_code& err, size_t bytes) {
+        do_read();
+    }
+    void do_read() {
+        async_read(sock_, buffer(read_buffer_),
+            MEM_FN2(read_complete, _1, _2), MEM_FN2(on_read, _1, _2));
+    }
+    void do_write(const std::string& msg) {
+        std::copy(msg.begin(), msg.end(), write_buffer_);
+        sock_.async_write_some(buffer(write_buffer_, msg.size()),
+            MEM_FN2(on_write, _1, _2));
+    }
+    size_t read_complete(const boost::system::error_code& err, size_t bytes) {
+        if (err) return 0;
+        bool found = std::find(read_buffer_, read_buffer_ + bytes, '\n') < read_buffer_ + bytes;
+        // we read one-by-one until we get to enter, no buffering
+        return found ? 0 : 1;
+    }
+private:
+    ip::tcp::socket sock_;
+    enum { max_msg = 1024 };
+    char read_buffer_[max_msg];
+    char write_buffer_[max_msg];
+    bool started_;
+};
+
+ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), 8001));
+
+void handle_accept(talk_to_client::ptr client, const boost::system::error_code& err) {
+    client->start();
+    talk_to_client::ptr new_client = talk_to_client::new_();
+    acceptor.async_accept(new_client->sock(), boost::bind(handle_accept, new_client, _1));
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+int main(int argc, char* argv[]) {
+    talk_to_client::ptr client = talk_to_client::new_();
+    acceptor.async_accept(client->sock(), boost::bind(handle_accept, client, _1));
+    service.run();
+}
