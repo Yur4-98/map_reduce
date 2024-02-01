aa19b05c6f573d70004ea31dc3941b833fb63b04
TblRka <92474441+TblRka@users.noreply.github.com>
2024-02-01 18:25:58 +0300

    размер файла

--- Project without MapReduce/binary.cpp
+++ Project without MapReduce/binary.cpp
@@ -9,14 +9,14 @@ void generate_file(char* file_path, std::vector<int> servers_num_vector)
     {
         input.server_num = servers_num_vector[std::rand() % servers_num_vector.size()];
         input.connections_count = std::rand() % CNT_MAX;
-        std::cout << "test" << input.server_num << '\n';
-        std::cout << "test" << input.connections_count << '\n';
+        //std::cout << "test" << input.server_num << '\n';
+        //std::cout << "test" << input.connections_count << '\n';
         fwrite(&input, sizeof(connection_log), 1, F);
     }
     fclose(F);
 }
 
-void test_output(char* file_path, std::vector<int> servers_num_vector)
+void test_output(char* file_path)
 {
     FILE* F = fopen(file_path, "rb+");
     connection_log output;
@@ -28,4 +28,13 @@ void test_output(char* file_path, std::vector<int> servers_num_vector)
         std::cout << '\n';
     }
     fclose(F);
+}
+
+long count_num(char* file_path)
+{
+    long nFileLen = 0;
+    struct _finddata_t fData;
+    int res = _findfirst(file_path, &fData);
+    if (res > 0) nFileLen = fData.size;
+    return nFileLen / (long)sizeof(connection_log);
 }
--- Project without MapReduce/binary.h
+++ Project without MapReduce/binary.h
@@ -4,9 +4,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <io.h>
 
 #define CNT_MAX 100
-#define OBS_MAX 10000
+#define OBS_MAX 110000
 
 typedef struct connection_log
 {
@@ -15,4 +16,5 @@ typedef struct connection_log
 } connection_log;
 
 void generate_file(char* file_path, std::vector<int> servers_num_vector);
-void test_output(char* file_path, std::vector<int> servers_num_vector);
+void test_output(char* file_path);
+long count_num(char* file_path);
